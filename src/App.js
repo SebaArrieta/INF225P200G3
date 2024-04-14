ff3dfbc4c3aa90070fcbb35cd51b0ad707069fd6
SebaArrieta <102710333+SebaArrieta@users.noreply.github.com>
2024-04-14 17:34:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import RegistrarHoras from "./components/RegistroHoras";
 import VerHora from "./components/VerHora";
 import RegistrarPaciente from "./components/RegistroPaciente";
 import RegistrarMedicos from "./components/RegistroMedicos";
+import Estadisticas from "./components/Estadisticas";
 
 function App() {
   return (
     <>
-      <Container className="mb-4">
+      <div className="mb-4">
         <main className="main-content form-signin w-100 m-auto">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -19,11 +20,13 @@ function App() {
             <Route path = "/verHora" element={<VerHora />}/>
             <Route path = "/registerPaciente" element={<RegistrarPaciente />}/>
             <Route path = "/registerMedico" element={<RegistrarMedicos />}/>
+            <Route path = "/Estadisticas" element={<Estadisticas />}/>
           </Routes>
         </main>
-      </Container>
+      </div>
     </>
   );
 }
 
 export default App;
+
