bd6cfeeb10143fd970570b9832022f78b24608f6
SebaArrieta <102710333+SebaArrieta@users.noreply.github.com>
2024-04-18 18:06:34 -0400

    Update personalController.js

--- server/Controllers/personalController.js
+++ server/Controllers/personalController.js
@@ -1,4 +1,5 @@
 const Personal = require("../models/Personal");
+const horas = require("../models/horas")
 
 // Controlador para registrar nuevo personal
 exports.registrarPersonal = async (req, res) => {
@@ -11,3 +12,55 @@ exports.registrarPersonal = async (req, res) => {
     res.status(500).json({ mensaje: "Error al registrar el personal" });
   }
 };
+
+const getHoras = async (tipo, init, end) => {
+  const query = {
+    fechaHora: {
+      $gte: init,
+      $lt: end,
+    },
+    tipoExamen: tipo
+  };
+
+  const result = await horas.countDocuments(query);
+  return result;
+}
+
+exports.generarEstadisticas = async (req, res) => {
+  try {
+    let init;
+    let end;
+
+    switch (req.query.tipo) {
+      case "Dia":
+          init = new Date(req.query.rango);
+          end = new Date(req.query.rango);
+
+          end.setHours(23, 59, 59);
+          console.log(init, end)
+          break;
+      case "Semana":
+          break;
+      case "Mes":
+          
+          break;
+      case "Año":
+          
+          break;
+  
+      default:
+          break;
+  }
+
+    res.json({
+      "Radiografia": await getHoras("Radiografía", init, end),
+      "Resonancia Magnética": await getHoras("Resonancia Magnética", init, end),
+      "Ecografías": await getHoras("Ecografías", init, end),
+      "Tomografías (TAC)": await getHoras("Tomografías (TAC)", init, end)
+    });
+
+  } catch (error) {
+    console.error('Error counting documents:', error);
+    throw error;
+  }
+}
