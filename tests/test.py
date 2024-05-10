e59a8c016982edd08e9d985d65a05e139cdee4f2
SebaArrieta <102710333+SebaArrieta@users.noreply.github.com>
2024-05-10 00:34:53 -0400

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,44 @@
+import unittest
+import requests
+
+class VerHoraTest(unittest.TestCase):
+    invalid_input = None
+    base_url = "http://localhost:5000/record/getHora/"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.invalid_input = {
+            'id': ''
+        }
+
+        cls.search_existing_id = {
+            'id': '6625b40078ccd1c959709ff2'
+        }
+    
+    @classmethod
+    def tearDownClass(cls):
+        del cls.invalid_input
+
+    def test_get_existing_id(self):
+        res = requests.get(self.base_url, params=self.search_existing_id)
+        hora = res.json()
+        hora_esperada = {
+            "apellido": "arrieta",
+            "fechaHora": "2024-04-21T12:30:00.000Z",
+            "nombre": "sebastian",
+            "nombreMedico": "ewfwef",
+            "observacionExamen": "wgqwgwqg",
+            "rut": 123456789,
+            "tipoExamen": "Radiografía",
+            "__v": 0,
+            "_id": "6625b40078ccd1c959709ff2"
+        }
+        self.assertEqual(hora_esperada, hora)
+
+    def test_invalid_search(self):
+        res = requests.get(self.base_url, params=self.invalid_input)
+        hora = res.json()['message']
+        self.assertEqual("Error obteniendo la hora", hora)
+
+if __name__ == '__main__':
+    unittest.main()
